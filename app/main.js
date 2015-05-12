de76283df35761b66af4a4aa6c8059d86fb72a4e
shyam <shyamsundar.shanmugam@wipro.com>
2015-05-12 14:15:31 +0530

    adding d3

--- app/main.js
+++ app/main.js
@@ -8,11 +8,12 @@ require.config({
   paths: {
     jquery: 'vendor/jquery/dist/jquery',
     bootstrap: 'vendor/bootstrap/dist/js/bootstrap',
-    handlebars: 'vendor/handlebars/handlebars'
+    handlebars: 'vendor/handlebars/handlebars',
+    d3: 'vendor/d3/d3'
   }
 });
 
-require(['jquery', 'bootstrap', './data', 'handlebars'], function($, bootstrap, frameworks, Handlebars) {
+require(['jquery', 'bootstrap', './data', 'handlebars', 'd3'], function($, bootstrap, frameworks, Handlebars, d3) {
   var m = {};
   // get unique elements in order
   var order = frameworks.map(function(r) {
@@ -32,4 +33,14 @@ require(['jquery', 'bootstrap', './data', 'handlebars'], function($, bootstrap, 
     page.append(template({ groupName: grp.replace(/-/g, ' '), frameworks: m[grp] }));
   });
 
+  var width = 500, height = 500;
+  var svg = d3.select('#right').append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('rect')
+      .attr("x",0)
+      .attr("y",0)
+      .attr("width",width)
+      .attr("height",height)
+      .style("fill","#e5f5f9");
 })
